85469c82e10394a02c6ad045ef7c010bb86b3e59
imhoyong <73011635+imhoyong@users.noreply.github.com>
2022-07-20 09:45:42 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,26 +1,8 @@
-const body = document.querySelector("body");
-const IMG_NUMBER = 6;
-
-function handleImgLoad(){
-    console.log("finished loading");
+function randomBackground(){
+    const num = Math.floor(Math.random() * 6) + 1; 
+  
+document.body.style.background = `url('images/${num}.jpg')`; 
+document.body.style.backgroundSize = "cover";
 }
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src = `images/${imgNumber+1}.jpg`;
-    image.classList.add("bgImage");
-    body.appendChild(image);
-    image.addEventListener("loaded", handleImgLoad);
-}
-
-function genRandom(){
-    const number = Math.floor(Math.random() * IMG_NUMBER);
-    return number;
-}
-
-function init(){
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
-}
-
-init();
+randomBackground();
